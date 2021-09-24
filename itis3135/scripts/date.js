523063298bd7bcae1b60910b65762a567ca9e7cc
Jpaint15 <85310137+Jpaint15@users.noreply.github.com>
2021-09-24 15:36:15 -0400

    Update

--- itis3135/scripts/date.js
+++ itis3135/scripts/date.js
@@ -1,8 +1,76 @@
 var username = prompt("What is your name?");
 var howareyou= prompt("How are you doing today");
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-console.log("Today is :" + today.toDateString() + "and the time is: " + time.toDateString());
-console.log("The Joking Dancing Panther co. welcomes you, Jpaint15 here!" );
-console.log("We're glad you are doing " + howareyou);
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var monthName;
+
+var hours = currentDate.getHours(); 
+var mins = currentDate.getMinutes(); 
+var secs = currentDate.getSeconds(); 
+var strToAppend;
+if (hours >12 )
+{
+    hours1 = "0" + (hours - 12);
+    strToAppend = "PM";
+}
+else if (hours <12)
+{
+    hours1 = "0" + hours;
+    strToAppend = "AM";
+}
+else
+{
+    hours1 = hours;
+    strToAppend = "PM";
+}
+
+if(mins<10)
+mins = "0" + mins;
+if (secs<10)
+secs = "0" + secs;
+
+switch (month)
+{
+    case 1:
+    monthName = "January";
+    break;
+    case 2:
+    monthName = "February";
+    break;
+    case 3:
+    monthName = "March";
+    break;
+    case 4:
+    monthName = "April";
+    break;
+    case 5:
+    monthName = "May";
+    break;
+    case 6:
+    monthName = "June";
+    break;
+    case 7:
+    monthName = "July";
+    break;
+    case 8:
+    monthName = "August";
+    break;
+    case 9:
+    monthName = "September";
+    break;
+    case 10:
+    monthName = "October";
+    break;
+    case 11:
+    monthName = "November";
+    break;
+    case 12:
+    monthName = "December";
+    break;
+}
+
+var year = currentDate.getFullYear();
+var myString;
+myString = "Today is " + day +  " - " + monthName + " - " + year + ".<br />Current time is " + hours1 + ":" + mins + ":" + secs + " " + strToAppend + ".";
+document.write(myString);
