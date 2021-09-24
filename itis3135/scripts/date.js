687ac26d544e3992cf000e7c3adc14a6070438f7
Jpaint15 <85310137+Jpaint15@users.noreply.github.com>
2021-09-24 15:08:17 -0400

    document

--- itis3135/scripts/date.js
+++ itis3135/scripts/date.js
@@ -3,6 +3,8 @@ var howareyou= prompt("How are you doing today");
 var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+document.open();
 document.write("Today is :" + today.toDateString() + "and the time is: " + time.toDateString());
 document.write("The Joking Dancing Panther co. welcomes you, Jpaint15 here!" );
 document.write("We're glad you are doing " + howareyou);
+document.close();
