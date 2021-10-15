45a8935c1fd30cd9953ae960d0ee54942072abe0
Jonathan Painter <jpaint15@uncc.edu>
2021-10-15 17:50:53 -0400

    S

--- practice/Practice.js
+++ practice/Practice.js
@@ -4,17 +4,17 @@ var $ = function(id) {
 
 window.onload = function() {
 	
-	//add onclick event handler for each image
 	
-	// for click event add item to order and update total
+	
+
 	
 	var total = 0;
 	
-	//onclick handler for item1
-	
-	document.getElementById("espresso").onclick=function(){
+
+		//onclick handler for item1
+	document.getElementById("espresso").onclick=function(){ //add onclick event handler for each image
 		
-		document.getElementById("order").append('$1.95 - Espresso - Delicious espresso. Wanna try it?');
+		document.getElementById("order").append('$1.95 - Espresso - Delicious espresso. Wanna try it?'); 	// for click event add item to order and update total
 		
 		total = (parseFloat(total) + parseFloat(1.95)).toFixed(2);
 		
@@ -22,18 +22,18 @@ window.onload = function() {
 		
 	};
 	
-	//onclick handler for item2
-	
+
+		//onclick handler for item2
 	document.getElementById("cappuccino").onclick=function(){
 		
 		document.getElementById("order").append('$3.45 - Cappuccino - Delicious Cappuccino.');
 		
 		total = (parseFloat(total) + parseFloat(3.45)).toFixed(2);
 		
-		document.getElementById("total").innerHTML="<em>Total:"+total+" </em>";
+		document.getElementById("total").innerHTML="<em>Total:"+total+" </em>"; // display order and total
 		
 	};
 	
-	// display order and total
 	
-}; // end onload
+	
+};
