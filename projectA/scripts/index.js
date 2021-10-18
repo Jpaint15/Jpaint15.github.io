03531b82ccb0fd24bc6714606bd70328b7b8658c
Jonathan Painter <jpaint15@uncc.edu>
2021-10-17 21:55:03 -0400

    INdex

--- projectA/scripts/index.js
+++ projectA/scripts/index.js
@@ -2,26 +2,26 @@ $('raptor').click(function(){
     window.location='https://raptor.martincarlisle.com' + this.id;
  })
 
- function ajax() {
-     const doc=new XMLHttpRequest();
-     doc.onload=function() {
-            developers(this);
-     }
-     doc.open("GET","people.xml");
-     doc.send();
- }
+function loadDoc() {
+  const xhttp = new XMLHttpRequest();
+  xhttp.onload = function() {
+    myFunction(this);
+  }
+  xhttp.open("GET", "info.xml");
+  xhttp.send();
+}
+function myFunction(xml) {
+  const xmlDoc = xml.responseXML;
+  const x = xmlDoc.getElementsByTagName("table");
+  let table="<tr><th>Name</th><th>Social</th></tr>";
+  for (let i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("social")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("people").innerHTML = table;
+}
 
- function developers(xml) {
-     const xmlDoc=xml.responseXML;
-     const x=xmlDoc.getElementByTagName("table");
-     let table="<tr><th>Name</th><th>Job</th></tr>";
-     for (let i=0;i,x.length;i++) {
-         table+="<tr><td>" +
-         x[i].getElementByTagName("name")[0].childNodes[0].nodeValue +
-         "</td><td>" +
-         x[i].getElementByTagName("social")[0].childNodes[0].nodeValue +
-         "</td><td>";
-     }
-     document.getElementById("people").innerHTML =table;
- }
  
