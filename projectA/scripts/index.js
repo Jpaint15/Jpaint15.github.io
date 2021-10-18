586eb22ad917470c50a4ce78085a666b92e9beb2
Jonathan Painter <jpaint15@uncc.edu>
2021-10-17 21:42:59 -0400

    S

--- projectA/scripts/index.js
+++ projectA/scripts/index.js
@@ -1,3 +1,26 @@
 $('raptor').click(function(){
     window.location='https://raptor.martincarlisle.com' + this.id;
  })
+
+ function ajax() {
+     const doc=new XMLHttpRequest();
+     doc.onload=function() {
+            developers(this);
+     }
+     doc.open("GET","people.xml");
+     doc.send();
+ }
+
+ function developers(xml) {
+     const xmlDoc=xml.responseXML;
+     const x=xmlDoc.getElementByTagName("table");
+     let table="<tr><th>Name</th><th>Job</th></tr>";
+     for (let i=0;i,x.length;i++) {
+         table+="<tr><td>" +
+         x[i].getElementByTagName("name")[0].childNodes[0].nodeValue +
+         "</td><td>" +
+         x[i].getElementByTagName("social")[0].childNodes[0].nodeValue +
+         "</td><td>";
+     }
+     document.getElementById("people").innerHTML =table;
+ }
