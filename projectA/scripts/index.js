163cabcccc7cfec2bfcd412fa44b92d3ad67e8b6
Jonathan Painter <jpaint15@uncc.edu>
2021-10-17 22:48:37 -0400

    Sda

--- projectA/scripts/index.js
+++ projectA/scripts/index.js
@@ -1,25 +1,25 @@
-
-function loadDoc() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    myFunction(this);
+function ajax() {
+    const doc = new XMLHttpRequest();
+    doc.onload = function() {
+      developers(this);
+    }
+    doc.open("GET", "people.xml");
+    doc.send();
   }
-  xhttp.open("GET", "people.xml");
-  xhttp.send();
-}
-function myFunction(xml) {
-  const xmlDoc = xml.responseXML;
-  const x = xmlDoc.getElementsByTagName("table");
-  let table="<tr><th>Name</th><th>Social</th></tr>";
-  for (let i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("social")[0].childNodes[0].nodeValue +
-    "</td></tr>";
+  function developers(xml) {
+    const xmlDoc = xml.responseXML;
+    const x = xmlDoc.getElementsByTagName("table");
+    var table="<tr><th>Name</th><th>Social</th></tr>";
+    for (var i = 0; i <x.length; i++) { 
+      table += "<tr><td>" +
+      x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+      "</td><td>" +
+      x[i].getElementsByTagName("social")[0].childNodes[0].nodeValue +
+      "</td></tr>";
+    }
+    document.getElementById("people").innerHTML = table;
   }
-  document.getElementById("people").innerHTML = table;
-}
+  
 
 $('raptor').click(function(){
     window.location='https://raptor.martincarlisle.com' + this.id;
