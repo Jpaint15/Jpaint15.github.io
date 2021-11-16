cb964ef9a3e0294fca68097e2b771f29e2fe9b11
Jonathan Painter <YourEmail@gmail.com>
2021-11-16 13:09:39 -0500

    x

---
+++ itis3135/Painter_Activity12/Painter_Activity12.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+    auto: true,
+    randomStart:true,
+    slideWidth: 250,
+    slideMargin: 10,
+    captions:true,
+    speed:2000,
+    pagerType:'short',
+    pagerSelector:'#pager'
+});
+});
