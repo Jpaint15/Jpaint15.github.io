bf8641538a83617c5ed41ddbe3806f2cc1fd1b0f
Jonathan Painter <YourEmail@gmail.com>
2021-11-19 12:26:15 -0500

    S

--- Activity14/Activity14.js
+++ Activity14/Activity14.js
@@ -1,25 +1,21 @@
 $(document).ready(function () {
-    //on click for <a> element
     $("#nav_list li").click(function() {
     var title = $(this).children("a").attr("title");
            var filename = title+".json";
-    consumeJSON(filename);
-    });
-    
+           consumeJSON(filename);
+        });
+        
     }); // end ready
     function consumeJSON(jsonFileURL) {
-    $.ajax({
-    url: "json_files/" + jsonFileURL,
-    //handle as text
-    dataType: "text",
-    success: function (data) {
-    //data downloaded + pass data
-    var json = $.parseJSON(data);
-    // display results
-    $("main > h2").html(json.speakers[0].month + "<br/>" + json.speakers[0].speaker);
-    $("main > h1").html(json.speakers[0].title);
-    $("main > img").attr("src", json.speakers[0].image);
-    $("main > p").html(json.speakers[0].text);
-    }
-    });
+        $.ajax({
+            url: "json_files/" + jsonFileURL,
+            dataType: "text",
+            success: function (data) {
+                var json = $.parseJSON(data);
+                $("main > h2").html(json.speakers[0].month + "<br/>" + json.speakers[0].speaker);
+                $("main > h1").html(json.speakers[0].title);
+                $("main > img").attr("src", json.speakers[0].image);
+                $("main > p").html(json.speakers[0].text);
+            }
+        });
     }
