b7e67ca158c4e2cc4a09170b207f3a8937365367
Jonathan Painter <YourEmail@gmail.com>
2021-11-25 15:32:12 -0500

    s

--- Activity15/Activity15.js
+++ Activity15/Activity15.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
-});
+    $.getJSON("facultyList.json", function(data) {
+        $.each(data,function() {
+            $.each(this,function(key, value) {
+                $('#faculty').append(
+                    "<img src=" + " ' " + value.image + " '><br>" +
+                    "<h3>" + value.full_name + "</h3>" +
+                    "<h3>" + value.title + "</h3>" +
+                    "<p>" + value.tag_line + "</p><br>" 
+                    );
+                });
+            });
+        });
+    });
