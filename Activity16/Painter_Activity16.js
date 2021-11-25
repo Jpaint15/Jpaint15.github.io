72a4e5abf116eca12462e5f9a736a82896941eb1
Jonathan Painter <YourEmail@gmail.com>
2021-11-25 15:55:23 -0500

    s

--- Activity16/Painter_Activity16.js
+++ Activity16/Painter_Activity16.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-
-});
+$(document).ready(function() {
+    // Create an method named 'ajax' to get the data from the
+ // JSON file
+    $.ajax({
+     type: "get",
+     url: "team.json",
+     beforeSend: function() {
+      $("#team").html("Loading...");
+     },
+     timeout: 10000,
+     error: function(xhr, status, error) {
+      alert("Error: " + xhr.status + " - " + error);
+     },
+     dataType: "json",
+     success: function(data) {
+      $("#team").html("");
+      $.each(data, function() {
+          $.each(this, function(key, value) {
+        $("#team").append(
+         "Name: " + value.name + "<br>" +
+         "Title: " + value.title + "<br>" +
+         "Bio: " + value.bio + "<br><br>"
+        );
+       });
+      });
+     }
+    });
+   });
