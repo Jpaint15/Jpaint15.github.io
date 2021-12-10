ddc9389fb6b826ec357a2228a2b4441a35edf79c
Jonathan Painter <YourEmail@gmail.com>
2021-12-10 13:47:41 -0500

    Commit

--- projectA/scripts/python.js
+++ projectA/scripts/python.js
@@ -17,10 +17,7 @@ $(document).ready(function() {
                 $.each(data, function() { 
                     $.each(this, function(key, value) { 
                         $("#programmer").append( 
-                            "<h3>" + value.full_name + "</h3>" +
-                            "<img src=" + " ' " + value.image + " '><br>" +
-                            "<h3>" + value.description + "</h3>" +
-                            "<p>" + value.follow + "</p><br>" 
+                            "<h3>" + value.description + "</h3>" 
                             
                             )
                         })
