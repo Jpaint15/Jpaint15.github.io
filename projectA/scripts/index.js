cbb800a24b438f1455ff5724d5e585391bb58cfe
Jonathan Painter <YourEmail@gmail.com>
2021-12-10 12:43:30 -0500

    Python Updaet

--- projectA/scripts/index.js
+++ projectA/scripts/index.js
@@ -25,4 +25,18 @@ $('raptor').click(function(){
     window.location='https://raptor.martincarlisle.com' + this.id;
  })
 
+ $(document).ready(function(){
+  $.getJSON("Programmers.json", function(data) {
+      $.each(data,function() {
+          $.each(this, function(key, value) {
+              $('#programmers').append(
+                  "<img src=" + " ' " + value.image + " '><br>" +
+                  "<h3>" + value.full_name + "</h3>" +
+                  "<h3>" + value.description + "</h3>" +
+                  "<p>" + value.follow + "</p><br>" 
+                  );
+              });
+          });
+      });
+  });
  
